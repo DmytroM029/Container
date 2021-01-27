7712c6cfbfb07a330dd66971776d6af43c09c456
Dmytro Marchenko <arif007@ukr.net>
2021-01-27 15:41:25 +0200

    Pain

--- js/slide.js
+++ js/slide.js
@@ -3,18 +3,40 @@ $(document).ready(function () {
          dots: false,
     speed: 300,
     arrows: true,
-    infinite: true,
+      infinite: true,
+      asNavFor: '.slider-second',
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+   arrows: false,
+        }
+      }
+       ]
    
   });
   $('.slider-second').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToShow: 6,
+  slidesToScroll: 1,
     asNavFor: '.rev_slider',
     dots: true,
     centerMode: false,
+    
     focusOnSelect: true,
-    variableWidth:true,
+    variableWidth:false,
     arrows: false,
+    infinite: true,
+
+    responsive: [
+    {
+      breakpoint: 767,
+        settings: {
+        slidesToShow: 3,
+  slidesToScroll: 1,
+    arrows: true,
+        }
+      }
+       ]
 
   });
 
